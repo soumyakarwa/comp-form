876cf82880f1ea4fb0a82085990246fc5b9b348c
soumyakarwa <soumyakarwa01@gmail.com>
2024-10-09 10:44:05 -0400

    sketch 2

--- pixel/sketch2/sketch.js
+++ pixel/sketch2/sketch.js
@@ -1,39 +1,50 @@
-let lightPos = {
-  topLeft: [50, 0],
-  topRight: [80, 0],
-  bottomLeft: [0, 300],
-  bottomRight: [140, 300],
-};
-let canvasDimensions = { width: 700, height: 300 };
-let lightWidth = { min: 20, max: 80 };
+const canvasDimensions = { width: 500, height: 500 };
+const center = { x: 300, y: 100 };
+const radius = 5;
+let blues, whiteColor;
 
 function setup() {
   createCanvas(canvasDimensions.width, canvasDimensions.height);
+  blues = {
+    sky: color(202, 240, 248),
+    light: color(63, 183, 217),
+    navy: color(17, 69, 129),
+  };
+  whiteColor = color(255, 255, 255);
 }
 
 function draw() {
   background(0);
-  noStroke();
-  fill(244, 240, 134);
-  quad(
-    lightPos.topLeft[0],
-    lightPos.topLeft[1],
-    lightPos.topRight[0],
-    lightPos.topRight[1],
-    lightPos.bottomRight[0],
-    lightPos.bottomRight[1],
-    lightPos.bottomLeft[0],
-    lightPos.bottomLeft[1]
-  );
+  img = createImage(canvasDimensions.width, canvasDimensions.height);
+  img.loadPixels();
 
-  setQuadCoordinates();
-}
+  for (let r = radius; r < 500; r += 3) {
+    for (let i = 0; i < 2 * Math.PI; i += Math.PI / 720) {
+      const x = center.x + r * Math.cos(i);
+      const y = center.y + r * Math.sin(i);
+      const rando = noise(r, i);
+      if (x > 0 && x < width && y > 0 && y < height) {
+        if (rando > 0.5) {
+          let c;
+          let colorNoise = noise(r);
+          if (colorNoise < 0.25) {
+            c = blues.light;
+          } else if (colorNoise < 0.5) {
+            c = blues.navy;
+          } else if (colorNoise < 0.7) {
+            c = blues.sky;
+          } else {
+            c = whiteColor;
+          }
 
-function setQuadCoordinates() {
-  if (mouseX > lightWidth.max && mouseX < width - lightWidth.max) {
-    lightPos.topLeft[0] = mouseX - lightWidth.min;
-    lightPos.topRight[0] = mouseX + lightWidth.min;
-    lightPos.bottomLeft[0] = mouseX - lightWidth.max;
-    lightPos.bottomRight[0] = mouseX + lightWidth.max;
+          img.set(x, y, c);
+        }
+      }
+    }
   }
+
+  img.updatePixels();
+  noSmooth();
+  image(img, 0, 0, width, height);
+  noLoop();
 }
